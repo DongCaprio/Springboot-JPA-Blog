450db0abd68327bba505d85a7ce2e9bc5e7cf0d1
김동진 <dongjin403@naver.com>
2022-05-02 18:54:32 +0900

    회원가입 처리(@Transactional)

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -10,9 +10,28 @@ let index = {
 			username:$("#username").val(),
 			password:$("#password").val(),
 			email:$("#email").val()
-		}
+		};
 		//console.log(data);
-		$.ajax().done().fail(); //ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청!
+		
+		//ajax호출시 default가 비동기 호출
+		//ajax통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청!
+		//ajax가 통신을 성공하고 서버가 json을 리턴해주면 
+		$.ajax({
+			//회원가입 수행 요청
+			type:"POST",
+			url:"/blog/api/user",
+			data: JSON.stringify(data),
+			contentType:"application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
+			dataType:"json" //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 객체로 변환해줌
+		}).done(function(resp){ 
+			//성공시
+			alert("회원가입이 완료되었습니다.")
+			//console.log(resp);
+			location.href="/blog";
+		}).fail(function(error){
+			//실패시
+			alert(JSON.stringify(error));
+		}); 
 	}
 }
 
