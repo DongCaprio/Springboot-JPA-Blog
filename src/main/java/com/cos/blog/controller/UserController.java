51aea6997ad753384ca18255c6cd343138d4916e
김동진 <dongjin403@naver.com>
2022-05-08 21:36:24 +0900

    ..

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
 	
 	@GetMapping("/auth/joinForm")
 	public String joinForm() {
-		return "user/joinForm";
+		return "user/joinForm"; 
 	}
 	
 	@GetMapping("/auth/loginForm")
