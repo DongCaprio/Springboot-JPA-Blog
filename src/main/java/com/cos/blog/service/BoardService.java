23a548d8c6626be33bea303f6978757d72381813
김동진 <dongjin403@naver.com>
2022-05-09 19:13:17 +0900

    글쓰기 처리완료, 글 상세보기 및 버튼추가

--- src/main/java/com/cos/blog/service/BoardService.java
+++ src/main/java/com/cos/blog/service/BoardService.java
@@ -1,24 +1,27 @@
 package com.cos.blog.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cos.blog.model.Board;
-import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.BoardRepository;
-import com.cos.blog.repository.UserRepository;
 
 @Service
 public class BoardService {
 	@Autowired
 	private BoardRepository boardRepository;
+	
+	public Board 글상세보기(int id) {
+		return boardRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 상세보기 실패 : 아이드를 찾을 수 없습니다");
+				});
+		
+	}
 
 	@Transactional
 	public void 글쓰기(Board board, User user) { // title, content
