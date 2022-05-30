bc1865871c681d900c3e84942ea9a76b2208ab26
김동진 <dongjin403@naver.com>
2022-05-30 19:45:38 +0900

    이해1

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -63,7 +63,7 @@ let index = {
 			contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
 			dataType: "json" //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => javascript 객체로 변환해줌
 		}).done(function(resp) {
-			alert("글수정가 완료되었습니다.")
+			alert("글수정이 완료되었습니다.")
 			location.href = "/";
 		}).fail(function(error) {
 			alert(JSON.stringify(error));
